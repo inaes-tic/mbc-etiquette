95fd530db4a8ab470401673e81a49c209fe9d194
Leonardo Vidarte <lvidarte@gmail.com>
2013-09-03 15:45:21 -0300

    Fixed some bugs

--- public/js/editor.js
+++ public/js/editor.js
@@ -96,12 +96,21 @@ $(document).ready(function() {
         ktext.id = Date.now();
         //window.ktext = ktext;
 
+        ktext.on('dragstart', function() {
+            this.old_x = Math.round(this.getX());
+            this.old_y = Math.round(this.getY());
+            console.log('dragstart: from ' +this.old_x + ',' + this.old_y);
+        });
+
         ktext.on('dragend', function() {
             var w = this.getWidth();
             var h = this.getHeight();
-            var x = this.getX();
-            var y = this.getY();
-            move(this.id, x, y, '');
+            var x = Math.round(this.getX());
+            var y = Math.round(this.getY());
+            x_steps = x - this.old_x;
+            y_steps = y - this.old_y;
+            move(this.id, x_steps, y_steps, '');
+            console.log('dragend: to ' + x + ',' + y);
             $('#info').html('<pre>text: ' + text + '\nid: ' + this.id +
                             '\nwidth: ' + w + '\nheight: ' + h + '\nx: ' + x +
                             '\ny: ' + y + '\n</pre>');
@@ -171,14 +180,24 @@ $(document).ready(function() {
         addAnchor(group, image.width, image.height, 'bottomRight');
         addAnchor(group, 0, image.height, 'bottomLeft');
 
+        group.on('dragstart', function() {
+            var i = this.get('.image')[0];
+            i.old_x = Math.round(this.getX());
+            i.old_y = Math.round(this.getY());
+            console.log('dragstart: from ' + i.old_x + ',' + i.old_y);
+        });
+
         group.on('dragend', function() {
             var i = this.get('.image')[0];
             var name = i.attrs.image.name;
             var w = i.attrs.width;
             var h = i.attrs.height;
-            var x = this.getX();
-            var y = this.getY();
-            move(i.id, x, y, '');
+            var x = Math.round(this.getX())
+            var y = Math.round(this.getY());
+            x_steps = x - i.old_x;
+            y_steps = y - i.old_y;
+            move(i.id, x_steps, y_steps, '');
+            console.log('dragend: to ' + x + ',' + y);
             $('#info').html('<pre>image: ' + name + '\nid: ' + i.id +
                             '\nwidth: ' + w + '\nheight: ' + h + '\nx: ' + x +
                             '\ny: ' + y + '\n</pre>');
@@ -191,8 +210,8 @@ $(document).ready(function() {
         var formdata = new FormData();
         formdata.append('images', kimage.attrs.image.name);
         formdata.append('id', kimage.id);
-        formdata.append('top', x);
-        formdata.append('left', y);
+        formdata.append('top', y);
+        formdata.append('left', x);
         formdata.append('bottom', '');
         formdata.append('right', '');
         formdata.append('width', kimage.attrs.image.width);
@@ -212,8 +231,8 @@ $(document).ready(function() {
     var move = function(id, x, y, duration) {
         var formdata = new FormData();
         formdata.append('elements', id);
-        formdata.append('x', x);
-        formdata.append('y', y);
+        formdata.append('x', x + 'px');
+        formdata.append('y', y + 'px');
         formdata.append('duration', duration);
         $.ajax({
             url: 'move',
