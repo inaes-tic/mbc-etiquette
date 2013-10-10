efddf2553983d1c8ad324b1d022239bc33fdfd41
José Luis Di Biase <josx@interorganic.com.ar>
2013-10-10 16:46:18 -0300

    Managing translaions
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- Makefile
+++ Makefile
@@ -1,5 +1,12 @@
 export NODE_CONFIG_DIR ?= $(PWD)/node_modules/mbc-common/config
 
+mos: locale/es/LC_MESSAGES/messages.mo
+
+locale/es/LC_MESSAGES/messages.mo:
+	./bin/extract_po.sh
+	./bin/update_languages.sh
+	./bin/compile-json locale locale
+
 all: update serve
 
 node_modules:
@@ -8,7 +15,7 @@ node_modules:
 npm:
 	npm install
 
-update: npm
+update: npm mos
 
 serve: update
 	node server.js
---
+++ bin/extract_po.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# syntax:
+# extract-po.sh
+
+# No -j on first line, to clear out .pot file (Issue#1170)
+mkdir -p locale/templates/LC_MESSAGES 
+
+# messages.po is server side strings
+xgettext --keyword=_ -L Perl --output-dir=locale/templates/LC_MESSAGES --from-code=utf-8 --output=messages.pot\
+ `find routes -name '*.js' | grep -v 'i18n.js'`
+xgettext -j -L Perl --output-dir=locale/templates/LC_MESSAGES --from-code=utf-8 --output=messages.pot `find views -name '*.jade'`
+#xgettext -j -L Perl --output-dir=locale/templates/LC_MESSAGES --from-code=utf-8 --output=messages.pot `find models -name '*.js'`
+
+# i18n-abide supports client-side gettext too. Usually you won't need this, unless your doing some
+# fancy new fangled webapp.
+#
+# client.po, assuming you have gettext strings in your client files (.js, .ejs)
+# js
+xgettext -L Perl --output-dir=locale/templates/LC_MESSAGES --from-code=utf-8 --output=client.pot \
+`find public -name '*.js' | grep -v 'gettext.js'`
+
+# ejs
+# xgettext -j -L PHP --keyword=_ --output-dir=locale/templates/LC_MESSAGES --output=client.pot `find client -name '*.ejs'`
