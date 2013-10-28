6cceb2d925ddfeffec9160d4126a551d68350405
José Luis Di Biase <josx@interorganic.com.ar>
2013-10-28 15:51:07 -0300

    Custom bb router calling canNavigateAway like caspa
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- public/js/main.js
+++ public/js/main.js
@@ -13,8 +13,53 @@ var AppRouter = Backbone.Router.extend({
     },
 
     editor: function() {
-        new EditorView();
-    }
+        return new EditorView();
+    },
+
+    // Manually bind a single named route to a callback. For example:
+    //
+    //     this.route('search/:query/p:num', 'search', function(query, num) {
+    //       ...
+    //     });
+    //
+    route: function(route, name, callback) {
+        if (!_.isRegExp(route)) route = this._routeToRegExp(route);
+        if (_.isFunction(name)) {
+          callback = name;
+          name = '';
+        }
+        if (!callback) callback = this[name];
+        var router = this;
+        Backbone.history.route(route, function(fragment) {
+            var args = router._extractParameters(route, fragment);
+
+            var ok = function() {
+                if (callback) {
+                    router.currentView = callback.apply(router, args);
+                }
+                router.trigger.apply(router, ['route:' + name].concat(args));
+                router.trigger('route', name, args);
+                Backbone.history.trigger('route', router, name, args);
+                router.currentHash = Backbone.history.getHash();
+            };
+
+            var cancel = function() {
+                // XXX: keep this, otherwise the browser url will point somewhere else.
+                // we need to set the history fragment to the (now) previous location
+                // to avoid re-creating the current view when we change the browser url.
+                // router.currentHash is updated by us after a successfull route change.
+                Backbone.history.fragment = router.currentHash;
+                location.hash = router.currentHash;
+            };
+
+            if (router.currentView && router.currentView.canNavigateAway) {
+                router.currentView.canNavigateAway({ok: ok, cancel: cancel});
+            } else {
+                ok();
+            }
+        });
+        return this;
+    },
 });
 
 var lang = $('html')[0].lang;
