829401511ad79d44ba3437cef8946111d7eb2414
Leonardo Vidarte <lvidarte@gmail.com>
2014-01-12 22:21:59 -0300

    Fixed issue #28: We should use the Mosto.General.fps value in Animations frame rate

--- routes/index.js
+++ routes/index.js
@@ -123,6 +123,7 @@ module.exports = function(server) {
         remove(element.id);
         element.type = 'animation';
         element.options = req.body;
+        element.options.frameRate = mbc.config.Mosto.General.fps;
         element.options.image = url.format({
             protocol: req.protocol,
             host: req.get('host'),
